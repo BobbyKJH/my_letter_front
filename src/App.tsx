056e9d7d8efe25dba9186f3db31b351cc89c7e69
bobby <rlatnguscjsw@gmail.com>
2024-01-18 02:35:36 +0900

    Feat : Route
    - Route 연결
    - StyledEngineProvider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { StyledEngineProvider } from '@mui/styled-engine';
+
+import HomePage from "@/page/HomePage";
+/** Style */
+import AppStyled from "@/App.styled";
+
 
 const App: React.FC = () => {
 
   return (
-    <>
-      First
-    </>
+    <StyledEngineProvider injectFirst>
+        <BrowserRouter>
+          <AppStyled/>
+
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+          </Routes>
+
+        </BrowserRouter>
+    </StyledEngineProvider>
   )
 }
 
