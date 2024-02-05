2a8839cff0194761c219bae5d65972bb60dde49f
bobby <rlatnguscjsw@gmail.com>
2024-02-05 13:52:11 +0900

    Feat : Auth Route
    - Auth Route 생성

--- src/page/router/AuthRouter.tsx
+++ src/page/router/AuthRouter.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import { Outlet, Navigate } from "react-router-dom";
+/** Components */
+import Header from '@/components/common/Header';
 
-const AuthRouter: React.FC = () => {
-  return (
-    <>AuthRouter</>
-  )
-};
+const AuthRoutes: React.FC = () => {
+	return (
+		!sessionStorage.getItem("uuid") ?
+			<>
+				<Header/>
 
-export default AuthRouter;
+
+				<Outlet/>
+			</>
+			:
+			// @ts-ignore
+			<Navigate to={"/"} {...alert("로그인이 필요합니다.")}/>
+	)
+}
+
+export default AuthRoutes;
