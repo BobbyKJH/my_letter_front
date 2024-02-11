568a4a92a21c4407f69244ff940d6802a920bb5c
bobby <rlatnguscjsw@gmail.com>
2024-02-11 19:54:41 +0900

    Feat : Todo Page Remove
    - Todo Page 제거

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage: React.FC = () => {
   return (
-    <>HomePage</>
+    <>1HomePage</>
   )
 };
 
--- src/page/today/TodoPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const TodoPage: React.FC = () => {
-  return (
-    <>TodoPage</>
-  )
-};
-
-export default TodoPage;
